15b9017f8000eade2c42c0cf18a55d5154f09cfb
kartikk-k <hellokartikk@gmail.com>
2026-01-22 21:12:35 +0530

    add: og image to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,15 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "kartikk.io",
   description: "Design Engineer building exceptional products.",
+  openGraph: {
+    title: "kartikk.io",
+    description: "Design Engineer building exceptional products.",
+    images: [
+      {
+        url: "/og.png",
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
